a4af3eab02b8f6099c148c2414e33741437ed9e7
Riley Dallas <rldaggie@gmail.com>
2016-10-11 15:59:41 -0500

    Stub out compliment route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/random-compliment', function (req, res) {
+  // 1. Add 3 additional compliment objects to this array
+  var compliments = [
+    {
+      text: 'You are swell!'
+    }
+  ]
+
+  // 2. Pull a random compliment from the compliments array and set it to randomCompliment
+  var randomCompliment = null;
+
+  // Return randomCompliment as json to the front end
+  res.json(randomCompliment)
+})
+
 module.exports = router;
