c3770798bd91b0161740e3b9d211f51d9d3f273b
jordan wade <jordanwade2009@live.com>
2016-10-12 17:41:13 -0500

    all compliments complimented

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,24 @@
 // Front end js goes here
 $(document).ready(function () {
-  // Your ajax code goes here
+  loadCompliment()
+  $('#generateRandomCompliment').on('click', loadCompliment)
+  changeColors();
+  $('#generateRandomCompliment').on('click', changeColors)
+
+
 })
+
+var loadCompliment = function () {
+  $.ajax({
+    url: 'http://localhost:3000/random-compliment'
+  })
+  .done(function (data) {
+    $('#compliment').text(data.text);
+  })
+}
+
+var changeColors = function () {
+  var colors = ["#fd6c3b", "#4edacf","#65a576","#f2d83d","#ff0066","#0066ff", "#009900"];
+  var randomColor = colors[Math.floor(Math.random()*colors.length)];
+  $('body').css('background-color', randomColor)
+}
--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,36 @@ router.get('/random-compliment', function (req, res) {
   // 1. Add several compliment objects to this array
   var compliments = [
     {
-      text: 'This compliment is in routes/index.js'
+      text:'If you were born before sliced bread was created, you\'d be "the coolest thing since before sliced bread".'
+    },
+    {
+      text: 'Your shoes look like you just bought them.'
+    },
+    {
+      text: 'If you were a potato, you\'d be a cute potato.'
+    },
+    {
+      text: 'On a scale of 1-10, you\'re pretty cool.'
+    },
+    {
+      text: 'You have a face that most people wouldn\'t punch.'
+    },
+    {
+    text: 'Compared to everybody else, you\'d probably would have a smaller chance of being killed by an angry prostitute. '
+    },
+    {
+      text: 'This compliment page may have been made solely for you.'
+    },
+    {
+      text: 'You click that button like a attractive person would.'
+    },
+    {
+      text: 'You probably smell really nice.'
     }
   ]
 
   // 2. replace compliments[0] with a random compliment from the compliments array
-  var randomCompliment = compliments[0];
+  var randomCompliment = compliments[Math.floor(Math.random()*compliments.length)];
 
   // Return randomCompliment as json to the front end
   res.json(randomCompliment)
