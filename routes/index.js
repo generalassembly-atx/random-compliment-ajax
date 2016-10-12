4d8323a67a6386fc2e29d1647d2767a8e543e275
Connor Garber <czaneg@gmail.com>
2016-10-12 17:35:24 -0500

    hw complete

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,30 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/random-compliment', function (req, res) {
+router.get('/random-compliment', function(req, res) {
   // 1. Add several compliment objects to this array
   var compliments = [
     {
-      text: 'This compliment is in routes/index.js'
+      text: "Your instructors love you"
+    }, {
+      text: "High five = ^5"
+    }, {
+      text: "Amanda thinks you're wicked smart!"
+    }, {
+      text: "Britney Jo is sooo proud of you! :)"
+    }, {
+      text: "Sean would totally hire you."
+    }, {
+      text: "It's almost beer o'clock!"
     }
   ]
 
   // 2. replace compliments[0] with a random compliment from the compliments array
-  var randomCompliment = compliments[0];
+  Array.prototype.randomElement = function() {
+    return this[Math.floor(Math.random() * this.length)]
+  };
+
+  var randomCompliment = compliments.randomElement();
 
   // Return randomCompliment as json to the front end
   res.json(randomCompliment)
