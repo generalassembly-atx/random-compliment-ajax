c7a36a064438ff326fd0c8fc31b519792c83f380
sean lewis <seanmartinlewis@gmail.com>
2016-10-12 19:45:07 -0500

    randomized background color

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,18 +2,37 @@
 $(document).ready(function () {
   showInitialCompliment();
   $('button').on('click', generateCompliment)
+  $('button').on('click', changeBackgroundColor)
 })
 
 function generateCompliment(e) {
   e.preventDefault();
   console.log('generated compliment');
+  $.ajax({
+    url: 'http://localhost:3000/random-compliment',
+  }) .done(function (data) {
+    console.log(data);
+        $('#compliment').html(data.text)
+  })
+
 }
 
 function showInitialCompliment() {
   $.ajax({
     url: 'http://localhost:3000/random-compliment',
   }) .done(function (data) {
+    console.log(data);
         $('#compliment').html(data.text)
   })
+}
+
+function changeBackgroundColor(e) {
+    e.preventDefault();
+    console.log('color change');
+
+    var colors = ["#fd6c3b", "#4edacf","#65a576","#f2d83d"];
+    var randomColors = colors[Math.floor(Math.random()*colors.length)]
+
+    $('#randoColor').css('background-color', randomColors)
 
 }
