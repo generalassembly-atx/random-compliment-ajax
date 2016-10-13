f1bb5e91c33b8c4dfe51430c75a4abbc1b5ef4f3
David Leseten <dleseten@gmail.com>
2016-10-12 22:36:51 -0500

    colorful compliments

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -12,7 +12,9 @@ function loadCompliment(){
       url: 'http://localhost:3000/random-compliment',
     }).done(function(response){
         //console.log(response);
+        newBackground();
         $('h1#compliment').text(response.text);
+
     })
       .fail(function(header, message, code){
         console.log(message, code);
@@ -20,4 +22,10 @@ function loadCompliment(){
       .always(function(){
         //console.log('I heart zebras');
     })
+}
+function newBackground(){
+  var styleNum = Math.floor(Math.random() * 4) + 1;
+  var styleClass = "theme" + styleNum;
+  console.log(styleNum);
+  $('body').attr("class", styleClass);
 }
