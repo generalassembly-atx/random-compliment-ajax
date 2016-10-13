c92e168ad7663ebd4049d0c240bb5ad089d6f0b6
Nathan Jensby <njensby@gmail.com>
2016-10-12 19:33:13 -0500

    added loadCompliment function

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,8 +1,18 @@
 // Front end js goes here
 $(document).ready(function () {
   // Your ajax code goes here
+  loadCompliment();
+  
 })
 
-$.ajax({
-  url: "http://localhost:3000/random-compliment"
-})
+function loadCompliment() {
+
+  var options = {
+    url: "http://localhost:3000/random-compliment"
+  }
+  var results = $.ajax(options)
+  results.done(function(object){
+    var compliment = object.text;
+    $('#compliment').text(compliment);
+  })
+}
