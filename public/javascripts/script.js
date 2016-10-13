ace2c3fd704f06b3158b13126b22fea23a589d14
David Leseten <dleseten@gmail.com>
2016-10-12 22:02:04 -0500

    initial requirements done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,23 @@
 // Front end js goes here
 $(document).ready(function () {
-  // Your ajax code goes here
+  loadCompliment()
+  $('#generateRandomCompliment').on('click', newCompliment)
 })
+
+function newCompliment(e){
+  loadCompliment();
+}
+function loadCompliment(){
+  $.ajax({
+      url: 'http://localhost:3000/random-compliment',
+    }).done(function(response){
+        //console.log(response);
+        $('h1#compliment').text(response.text);
+    })
+      .fail(function(header, message, code){
+        console.log(message, code);
+    })
+      .always(function(){
+        //console.log('I heart zebras');
+    })
+}
