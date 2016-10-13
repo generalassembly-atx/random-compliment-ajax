db4659bc3a69a8729189bc8c850da6ad34fbe68d
Connor Garber <czaneg@gmail.com>
2016-10-12 21:19:50 -0500

    lil bit of css

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,14 +3,23 @@ $(document).ready(function() {
   // Your ajax code goes here
   $('body').css('background-color', colors.randomElement())
   loadRandom();
-  $('button').on('click', loadRandom);
+  $('#generateRandomCompliment').on('click', loadRandom);
 });
 
-Array.prototype.randomElement = function () {
+Array.prototype.randomElement = function() {
   return this[Math.floor(Math.random() * this.length)]
 };
 
-colors = ['#595AB7','#A57706','#D11C24','#C61C6F','#BD3613','#2176C7','#259286','#738A05']
+colors = [
+  '#595AB7',
+  '#A57706',
+  '#D11C24',
+  '#C61C6F',
+  '#BD3613',
+  '#2176C7',
+  '#259286',
+  '#738A05'
+]
 
 function loadRandom() {
   $.ajax({url: '/random-compliment'}).done(function(response) {
