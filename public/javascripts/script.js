3ecfe325f40e3a9b97a64adc8bd96aec0f0dbe81
Thomas Foster <thomasfoster90>
2016-10-12 20:10:13 -0500

    done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,24 @@
 // Front end js goes here
 $(document).ready(function () {
   // Your ajax code goes here
+  loadCompliment()
+  $('button').on('click', loadCompliment)
+  changeColors();
+  $('button').on('click', changeColors)
 })
+
+var loadCompliment= function () {
+  $.ajax({
+    url:'http://localhost:3000/random-compliment'
+  })
+  .done(function (data) {
+    // data=response;
+    $('#compliment').text(data.text);
+  })
+}
+
+var changeColors= function () {
+  var colors = ["#fd6c3b", "#4edacf","#65a576","#f2d83d"];
+  var randomColor = colors[Math.floor(Math.random()*colors.length)];
+  $('body').css('background-color', randomColor)
+}
