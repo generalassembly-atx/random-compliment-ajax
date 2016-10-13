53435957e2dfd564ed5a9d601fe324fc43472e41
sean lewis <seanmartinlewis@gmail.com>
2016-10-12 19:31:49 -0500

    displayed welcome compliment with ajax

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,9 +1,19 @@
 // Front end js goes here
 $(document).ready(function () {
+  showInitialCompliment();
   $('button').on('click', generateCompliment)
 })
 
 function generateCompliment(e) {
   e.preventDefault();
   console.log('generated compliment');
+}
+
+function showInitialCompliment() {
+  $.ajax({
+    url: 'http://localhost:3000/random-compliment',
+  }) .done(function (data) {
+        $('#compliment').html(data.text)
+  })
+
 }
